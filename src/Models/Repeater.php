442fb822bf0c36317d7a57d3abe77e06604caae1
Karel Brijs <karel@chuck.be>
2018-12-07 18:21:35 +0000

    update repeater model: add getJson() function

--- src/Models/Repeater.php
+++ src/Models/Repeater.php
@@ -18,4 +18,25 @@ class Repeater extends Eloquent
     protected $casts = [
         'json' => 'array',
     ];
+
+    public function getJson(string $string)
+    {
+        $json = $this->resolveJson($string);
+        return $json ? $json : null;
+    }
+
+    private function resolveJson($var)
+    {
+        $json = $this->json;
+        $split = explode('.', $var);
+        foreach ($split as $value) {
+            if(array_key_exists($value, $json)) {
+                $json = $json[$value];
+            } else {
+                return null;
+            }
+        }
+
+        return $json;
+    }
 }
