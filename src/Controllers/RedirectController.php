b4e05c496e8c80f55ddf8f1a7a020306481215fd
Karel Brijs <karel@chuck.be>
2019-03-05 20:39:39 +0000

    stylefixes RedirectController

--- src/Controllers/RedirectController.php
+++ src/Controllers/RedirectController.php
@@ -2,14 +2,17 @@
 
 namespace Chuckbe\Chuckcms\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
 use Chuckbe\Chuckcms\Models\Redirect;
 
-class RedirectController extends Controller
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class RedirectController extends BaseController
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
      * Create a new controller instance.
