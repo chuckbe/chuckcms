b4891075fd4acf835d5a2afb67de1831bcff22ad
KarelBrijs <karel@chuck.be>
2020-07-27 15:47:22 +0200

    update templates overview

--- src/views/backend/templates/index.blade.php
+++ src/views/backend/templates/index.blade.php
@@ -1,101 +1,65 @@
-@extends('chuckcms::backend.layouts.admin')
-
-@section('title')
-	Templates
-@endsection
-
-@section('add_record')
-	@can('create templates')
-	{{-- <a href="{{ route('dashboard.content.resources.create') }}" class="btn btn-link text-primary m-l-20 hidden-md-down">Voeg Nieuwe Template Toe</a> --}}
-	@endcan
-@endsection
-
-@section('breadcrumbs')
-	<ol class="breadcrumb">
-		<li class="breadcrumb-item active"><a href="{{ route('dashboard.templates') }}">Templates</a></li>
-	</ol>
-@endsection
-
-@section('css')
-	<link href="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/FixedColumns/css/dataTables.fixedColumns.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.chuck.be/assets/plugins/datatables-responsive/css/datatables.responsive.css" rel="stylesheet" type="text/css" media="screen" />
-@endsection
-
-@section('scripts')
-	<script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/js/jquery.dataTables.min.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/js/dataTables.bootstrap.js" type="text/javascript"></script>
-    <script src="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap.js" type="text/javascript"></script>
-    <script type="text/javascript" src="https://cdn.chuck.be/assets/plugins/datatables-responsive/js/datatables.responsive.js"></script>
-    <script type="text/javascript" src="https://cdn.chuck.be/assets/plugins/datatables-responsive/js/lodash.min.js"></script>
-    <script src="https://cdn.chuck.be/assets/js/tables.js" type="text/javascript"></script>
-@endsection
+@extends('chuckcms::backend.layouts.base')
 
 @section('content')
-<div class=" container-fluid   container-fixed-lg">
+<div class="container">
     <div class="row">
-		<div class="col-lg-12">
-		<!-- START card -->
-			<div class="card card-transparent">
-				<div class="card-header ">
-					<div class="card-title">Templates</div>
-					<div class="tools">
-						<a class="collapse" href="javascript:;"></a>
-						<a class="config" data-toggle="modal" href="#grid-config"></a>
-						<a class="reload" href="javascript:;"></a>
-						<a class="remove" href="javascript:;"></a>
-					</div>
-				</div>
-				<div class="card-block">
-					<div class="table-responsive">
-						<table class="table table-hover table-condensed" id="condensedTable">
-						<thead>
-							<tr>
-								<th style="width:5%">ID</th>
-								<th style="width:30%">Naam</th>
-								<th style="width:20%">Auteur</th>
-								<th style="width:10%">Versie</th>
-								<th style="width:10%">Type</th>
-								<th style="width:25%">Actions</th>
-							</tr>
-						</thead>
-							<tbody>
-								@foreach($templates as $tmp)
-								<tr>
-									<td class="v-align-middle">{{ $tmp->id }}</td>
-							    	<td class="v-align-middle">
-							    		@if($tmp->active == 1)
-					                		<i class="fa fa-circle" style="color:chartreuse;"></i>
-					                	@else
-					                		<i class="fa fa-circle" style="color:red;"></i>
-					                	@endif 
-					                	{{ $tmp->name }}
-							    	</td>
-							    	<td class="v-align-middle">{{ $tmp->author }}</td>
-							    	<td class="v-align-middle">(v{{ $tmp->version }})</td>
-							    	<td class="v-align-middle">{{ $tmp->type }}</td>
-							    	<td class="v-align-middle semi-bold">
-							    		@can('edit templates')
-							    		<a href="{{ route('dashboard.templates.edit', ['slug' => $tmp->slug]) }}" class="btn btn-default btn-sm btn-rounded m-r-20">
-							    			<i data-feather="edit-2"></i> edit
-							    		</a>
-							    		@endcan
-							    		@can('delete templates')
-							    		{{-- <a href="{{ route('dashboard.forms.delete', ['slug' => $tmp->slug]) }}" class="btn btn-default btn-sm btn-rounded m-r-20">
-							    			<i data-feather="trash"></i> delete
-							    		</a> --}}
-							    		@endcan
-							    	</td>
-							  	</tr>
-							  	@endforeach
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		<!-- END card -->
-		</div>
+        <div class="col-sm-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mt-3">
+                    <li class="breadcrumb-item active" aria-current="page">Templates</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <div class="row bg-light shadow-sm rounded p-3 mb-3 mx-1">
+        <div class="col-sm-12 my-3">
+            <div class="row">
+            	<div class="col-sm-6"><small>Naam</small></div>
+            	<div class="col-sm-3"><small>Auteur</small></div>
+            	<div class="col-sm-3"><small>Type</small></div>
+            	<div class="col-sm-12"><hr></div>
+            </div>
+            @foreach($templates as $tmp)
+            <div class="row">
+            	<div class="col-sm-6">
+            		@if($tmp->active == 1)
+            		<i class="fa fa-circle" style="color:chartreuse;"></i>
+                	@else
+            		<i class="fa fa-circle" style="color:red;"></i>
+                	@endif 
+                	{{ $tmp->name }} (v{{ $tmp->version }})
+            	</div>
+            	<div class="col-sm-3">{{ $tmp->author }}</div>
+            	<div class="col-sm-3">
+            		{{ $tmp->type }}
+            		@can('edit templates')
+		    		<a href="{{ route('dashboard.templates.edit', ['slug' => $tmp->slug]) }}" class="btn btn-sm btn-outline-secondary rounded float-right">
+		    			<i class="fa fa-pen"></i> edit
+		    		</a>
+		    		@endcan
+            	</div>
+            	<div class="col-sm-12"><hr></div>
+            </div>
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
+
+@section('css')
+	
+@endsection
+
+@section('scripts')
+
+@endsection
+
+
+
+
+
+
+
+
+
+
