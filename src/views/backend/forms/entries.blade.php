5e7f36938a35520475ce685939390315b9ffc968
rawaldeep <rawaldeep@chuck.be>
2020-10-21 15:50:25 +0000

    added new layout to forms entries

--- src/views/backend/forms/entries.blade.php
+++ src/views/backend/forms/entries.blade.php
@@ -4,6 +4,78 @@
 	Formulier Ingaves
 @endsection
 
+@section('content')
+<div class="container p-3">
+	<div class="row">
+		<div class="col-sm-12">
+			<nav aria-label="breadcrumb">
+                <ol class="breadcrumb mt-3">
+                    <li class="breadcrumb-item active" aria-current="Ingaves">Ingaves</li>
+                </ol>
+            </nav>
+		</div>
+	</div>
+	<div class="row bg-light shadow-sm rounded p-3 mb-3 mx-1">
+		<div class="tools">
+			<a class="collapse" href="javascript:;"></a>
+			<a class="config" data-toggle="modal" href="#grid-config"></a>
+			<a class="reload" href="javascript:;"></a>
+			<a class="remove" href="javascript:;"></a>
+		</div>
+		<div class="col-sm-12 my-3">
+			<div class="table-responsive">
+				<table class="table table-condensed table-detailed" data-datatable id="detailTable" style="width:100%">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Slug</th>
+							<th scope="col">Date</th>
+							<th scope="col">Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($entries as $entry)
+							<tr>
+								<td>{{ $entry->id }}</td>
+							    <td class="semi-bold">{{ $form->slug }}</td>
+							    <td class="semi-bold">{{ date('d/m/Y - H:i:s', strtotime($entry->created_at)) }}</td>
+							    <td class="semi-bold">
+							    	@can('show formentries')
+							    		<a href="{{ route('dashboard.forms.entry', ['slug' => $form->slug, 'id' => $entry->id]) }}" class="btn btn-default btn-sm btn-rounded m-r-20">
+							    			<i class="fa fa-edit"></i> bekijken
+							    		</a>
+							    	@endcan
+							    </td>
+							</tr>
+							<tr class="row-details collapse">
+								<td colspan="4">
+									<table class="table table-inline">
+										<tbody>
+											@foreach($entry->entry as $entryKey => $entryValue)
+			                          			<tr>
+			                          				<td>{{ $entryKey }}</td>
+			                          				<td>
+			                          					@if(is_array($entryValue))
+			                          						{!! implode('<br>', $entryValue) !!}
+			                          						@else
+			                          						{{ $entryValue }}
+														@endif
+			                          				</td>
+			                          			</tr>
+			                          		@endforeach
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+
 @section('css')
 	{{-- <link href="https://cdn.chuck.be/assets/plugins/jquery-datatable/media/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" /> --}}
     <link href="https://cdn.chuck.be/assets/plugins/jquery-datatable/extensions/FixedColumns/css/dataTables.fixedColumns.min.css" rel="stylesheet" type="text/css" />
@@ -61,76 +133,3 @@ $.fn.dataTable.ext.errMode = 'none';
     </script>
 @endsection
 
-@section('content')
-<div class=" container-fluid   container-fixed-lg">
-<div class="container p-3">
-    <div class="row">
-		<div class="col-lg-12">
-		<!-- START card -->
-			<div class="card card-transparent">
-				<div class="card-header ">
-					<div class="card-title">Ingaves</div>
-					<div class="tools">
-						<a class="collapse" href="javascript:;"></a>
-						<a class="config" data-toggle="modal" href="#grid-config"></a>
-						<a class="reload" href="javascript:;"></a>
-						<a class="remove" href="javascript:;"></a>
-					</div>
-				</div>
-				<div class="card-block">
-					<div class="table-responsive">
-						<table class="table table-hover table-condensed table-detailed" id="detailTable">
-						<thead>
-							<tr>
-								<th style="width:5%">ID</th>
-								<th style="width:30%">Slug</th>
-								<th style="width:30%">Date</th>
-								<th style="width:35%">Actions</th>
-							</tr>
-						</thead>
-							<tbody>
-								@foreach($entries as $entry)
-								<tr>
-									<td class="v-align-middle">{{ $entry->id }}</td>
-							    	<td class="v-align-middle semi-bold">{{ $form->slug }}</td>
-							    	<td class="v-align-middle semi-bold">{{ date('d/m/Y - H:i:s', strtotime($entry->created_at)) }}</td>
-							    	<td class="v-align-middle semi-bold">
-							    		@can('show formentries')
-							    		<a href="{{ route('dashboard.forms.entry', ['slug' => $form->slug, 'id' => $entry->id]) }}" class="btn btn-default btn-sm btn-rounded m-r-20">
-							    			<i data-feather="edit-2"></i> bekijken
-							    		</a>
-							    		@endcan
-							    	</td>
-							  	</tr>
-								  	<tr class="row-details collapse">
-			                          	<td colspan="4">
-			                          		<table class="table table-inline">
-			                          			<tbody>
-			                          				@foreach($entry->entry as $entryKey => $entryValue)
-			                          				<tr>
-			                          					<td>{{ $entryKey }}</td>
-			                          					<td>
-			                          						@if(is_array($entryValue))
-			                          						{!! implode('<br>', $entryValue) !!}
-			                          						@else
-			                          						{{ $entryValue }}@endif
-			                          					</td>
-			                          				</tr>
-			                          				@endforeach
-			                          			</tbody>
-			                          		</table>
-			                          	</td>
-		                          	</tr>
-							  	@endforeach
-							</tbody>
-						</table>
-
-					</div>
-				</div>
-			</div>
-		<!-- END card -->
-		</div>
-    </div>
-</div>
-</div>
-@endsection
