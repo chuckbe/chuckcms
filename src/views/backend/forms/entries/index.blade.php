3ef4dad4572d6cb660ead3be58e69cfaba4334d4
rawaldeep <rawaldeep@chuck.be>
2020-12-22 09:24:10 +0000

    added new layout to form entries

--- src/views/backend/forms/entries/index.blade.php
+++ src/views/backend/forms/entries/index.blade.php
@@ -1,7 +1,51 @@
-@extends('chuckcms::backend.layouts.admin')
+@extends('chuckcms::backend.layouts.base')
 
 @section('title')
 	Formulier '{{ $form->slug }}' Ingave #{{ $entry->id}}
+@endsection
+
+@section('content')
+<div class="container p-3">
+	<div class="row">
+		<div class="col-sm-12">
+			<nav aria-label="breadcumb">
+				<ol class="breadcrumb mt-3">
+					<li class="breadcrumb-item active" aria-current="Ingaves">Ingaves</li>
+				</ol>
+			</nav>
+		</div>
+	</div>
+	<div class="row bg-light shadow-sm rounded p-3 mb-3 mx-1">
+		<div class="col-sm-12 my-3">
+			<div class="table-responsive">
+				<table class="table" data-datatable style="width:100%">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Veld</th>
+							<th scope="col">Waarde</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($entry->entry as $entryKey => $entryValue)
+							<tr class="page_line" data-id="{{ $loop->iteration }}">
+								<td>{{ $loop->iteration }}</td>
+								<td class="semi-bold">{{ $entryKey }}</td>
+								<td class="v-align-middle semi-bold">
+									@if(is_array($entryValue))
+                  						{!! implode('<br>', $entryValue) !!}
+                  						@else
+                  						{{ $entryValue }}
+									@endif
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
 
 @section('css')
@@ -10,47 +54,4 @@
 
 @section('scripts')
 
-@endsection
-
-@section('content')
-<div class=" container-fluid   container-fixed-lg">
-    <div class="row">
-		<div class="col-lg-12">
-		<!-- START card -->
-			<div class="card card-transparent">
-				<div class="card-header ">
-					<div class="card-title">Ingaves</div>
-				</div>
-				<div class="card-block">
-					<div class="table-responsive">
-						<table class="table table-hover table-condensed" data-datatable style="width:100%">
-						<thead>
-							<tr>
-								<th style="width:5%">ID</th>
-								<th style="width:35%">Veld</th>
-								<th style="width:60%">Waarde</th>
-							</tr>
-						</thead>
-							<tbody>
-								@foreach($entry->entry as $entryKey => $entryValue)
-								<tr>
-									<td class="v-align-middle">{{ $loop->iteration }}</td>
-									<td class="v-align-middle">{{ $entryKey }}</td>
-							    	<td class="v-align-middle semi-bold">
-							    		@if(is_array($entryValue))
-                  						{!! implode('<br>', $entryValue) !!}
-                  						@else
-                  						{{ $entryValue }}@endif
-							    	</td>
-							  	</tr>
-							  	@endforeach
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		<!-- END card -->
-		</div>
-    </div>
-</div>
 @endsection
