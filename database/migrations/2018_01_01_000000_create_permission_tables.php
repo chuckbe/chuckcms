5a0fb79a5cdd75bc30266c632ed4068a412f6ebe
rawaldeep <rawaldeep@chuck.be>
2022-01-17 10:53:32 +0000

    found issue in migration create_permission_table

--- database/migrations/2018_01_01_000000_create_permission_tables.php
+++ database/migrations/2018_01_01_000000_create_permission_tables.php
@@ -30,7 +30,7 @@ class CreatePermissionTables extends Migration
         });
 
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames) {
-            $table->increments('id');
+            $table->bigInteger('id');
             $table->unsignedInteger('permission_id');
             $table->morphs('model');
 
@@ -43,7 +43,7 @@ class CreatePermissionTables extends Migration
         });
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames) {
-            $table->increments('id');
+            $table->bigInteger('id');
             $table->unsignedInteger('role_id');
             $table->morphs('model');
 
@@ -56,7 +56,7 @@ class CreatePermissionTables extends Migration
         });
 
         Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
-            $table->increments('id');
+            $table->bigInteger('id');
             $table->unsignedInteger('permission_id');
             $table->unsignedInteger('role_id');
 
